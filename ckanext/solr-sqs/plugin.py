aa82710ca750b4d8b24800d1488ee510d29f633e
Yizhao Tan <ytan@j2xm0900990.wkstn.toronto.ca>
2019-11-14 09:48:52 -0500

    Implemented SQS msg send

--- ckanext/solr-sqs/plugin.py
+++ ckanext/solr-sqs/plugin.py
@@ -1,6 +1,34 @@
+from ckan.common import config
+
+import boto3
+
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+import logging
+logger = logging.getLogger('ckan.base.logic')
+
+def _send_message(msg):
+    client = boto3.client('sqs', 'us-east-1')
+
+    sqs_url = config.get('ckan.sqs_solr_sync_queue_url')
+
+    client.send_message(
+        QueueUrl=sqs_url,
+        MessageBody=msg
+    )
 
 class SolrSqsPlugin(plugins.SingletonPlugin):
-    pass
+    p.implements(p.IPackageController, inherit=True)
+
+    def after_create(context, pkg_dict):
+        logger.warn(('create', pkg_dict['id']))
+        _send_message(pkg_dict['id'])
+
+    def after_update(context, pkg_dict):
+        logger.warn(('update', pkg_dict['id']))
+        _send_message(pkg_dict['id'])
+
+    def after_delete(context, pkg_dict):
+        logger.warn(('delete', pkg_dict['id']))
+        _send_message(pkg_dict['id'])
