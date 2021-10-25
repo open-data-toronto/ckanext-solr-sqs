8b74d91ea49a593b95ee799371d7c1b016582529
Carlos <carlos.hernandez@toronto.ca>
2021-10-25 16:55:14 -0400

    Updated refresh command for CKAN 2.9

--- ckanext/solr_sqs/receiver.py
+++ ckanext/solr_sqs/receiver.py
@@ -1,40 +1,38 @@
-from ConfigParser import ConfigParser
-
-import subprocess
 import re
+import subprocess
 
 import boto3
+from ConfigParser import ConfigParser
 
 
 def get_url():
     config = ConfigParser()
-    config.read('/etc/ckan/default/production.ini')
+    config.read("/etc/ckan/default/production.ini")
 
-    return config.get('app:main', 'ckan.sqs_solr_sync_queue_url')
+    return config.get("app:main", "ckan.sqs_solr_sync_queue_url")
+
 
 def receive_messages():
     sqs_url = get_url()
-    region = re.search("sqs.(.*).amazon", sqs_url).group(1) 
-    client = boto3.client('sqs', region)
+    region = re.search("sqs.(.*).amazon", sqs_url).group(1)
+    client = boto3.client("sqs", region)
 
     response = client.receive_message(QueueUrl=sqs_url, MaxNumberOfMessages=10)
-    messages = response.get('Messages', [])
+    messages = response.get("Messages", [])
 
     if len(messages):
-        subprocess.call([
-            'paster',
-            '--plugin=ckan',
-            'search-index',
-            'rebuild',
-            '-r',
-            '--config=/etc/ckan/default/production.ini'
-        ])
-
-    for m in messages:
-        client.delete_message(
-            QueueUrl=sqs_url,
-            ReceiptHandle=m['ReceiptHandle']
+        subprocess.call(
+            [
+                "ckan",
+                '--config="/etc/ckan/default/production.ini' "search-index",
+                "rebuild",
+                "-r",
+            ]
         )
 
-if __name__ == '__main__':
+    for m in messages:
+        client.delete_message(QueueUrl=sqs_url, ReceiptHandle=m["ReceiptHandle"])
+
+
+if __name__ == "__main__":
     receive_messages()
