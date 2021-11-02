75e17ea4a64d755f486486c223dbf61b3f51a8e6
Mackenzie Nichols <mackey.nichols@gmail.com>
2021-11-02 13:39:52 -0400

    Adding 'solr_reindex_message' endpoint to allow for manual solr reindex, tweaking what is sent in messages

--- ckanext/solr_sqs/plugin.py
+++ ckanext/solr_sqs/plugin.py
@@ -1,10 +1,18 @@
 from ckan.common import config
+import ckan.plugins.toolkit as tk
 
 import boto3
 import re
 
 import ckan.plugins as p
 
+@tk.side_effect_free
+def send_message_api(context, data_dict):
+    # make sure an authorized user is making this call
+    assert context["auth_user_obj"], "This endpoint can be used by authorized accounts only"
+
+    _send_message(data_dict["package_id"])
+    return data_dict
 
 def _send_message(msg):
     sqs_url = config.get('ckan.sqs_solr_sync_queue_url')
@@ -20,10 +28,17 @@ class SolrSqsPlugin(p.SingletonPlugin):
     p.implements(p.IPackageController, inherit=True)
 
     def after_create(self, context, pkg_dict):
-        _send_message(pkg_dict['id'])
+        _send_message(pkg_dict['name'])
 
     def after_update(self, context, pkg_dict):
-        _send_message(pkg_dict['id'])
+        _send_message(pkg_dict['name'])
 
     def after_delete(self, context, pkg_dict):
-        _send_message(pkg_dict['id'])
+        _send_message(pkg_dict['name'])
+
+
+    p.implements(p.IActions)
+    def get_actions(self):
+        return {
+            "solr_reindex_message": send_message_api
+        }
