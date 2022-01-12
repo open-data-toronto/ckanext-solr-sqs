00fdd6a8745251e986c15d071164e8a396b99d53
Mackenzie Nichols <mackey.nichols@gmail.com>
2022-01-12 09:23:59 -0500

    Limit 10 msgs/run to avoid parallel receiver runs

--- ckanext/solr_sqs/receiver.py
+++ ckanext/solr_sqs/receiver.py
@@ -27,9 +27,13 @@ def receive_messages():
     client = boto3.client("sqs", region)
 
     # there are a maximum of 10 messages fetched in client.receive_message
-    # so we fetch the sqs messages 10 at a time until there are none left
+    # we only process 10 messages per run of this script because this script
+    # runs every 5 minutes and we dont want to have it running multiple times
+    # in parallel
+
     response = client.receive_message(QueueUrl=sqs_url, MaxNumberOfMessages=10)
     messages = response.get("Messages", [])
+    print(messages)
 
     if len(messages):
         # Before looping through all messages, refresh the whole solr index once for good measure
@@ -41,33 +45,27 @@ def receive_messages():
                 "rebuild",
                 "-r"
             ])
+       
+    processed = []
+    # for each message in the queue, recreate the package in the message's index
+    for message in messages:
+        # if we've already reindexed a package in this run, dont do it again
+        if message["Body"] in processed:
+            continue
 
-    while len(messages) > 0:
-            
-        processed = []
-        # for each message in the queue, recreate the package in the message's index
-        for message in messages:
-            # if we've already reindexed a package in this run, dont do it again
-            if message["Body"] in processed:
-                continue
+        processed.append(message["Body"])
 
-            processed.append(message["Body"])
-
-            subprocess.call(
-                [
-                    "ckan",
-                    "--config=/etc/ckan/default/production.ini",
-                    "search-index",
-                    "rebuild",
-                    message["Body"]
-                ]
-            )
-            # delete the message from the queue
-            client.delete_message(QueueUrl=sqs_url, ReceiptHandle = message["ReceiptHandle"]) 
-        
-        # fetch the next batch of messages and start the cycle again
-        response = client.receive_message(QueueUrl=sqs_url, MaxNumberOfMessages=10)
-        messages = response.get("Messages", [])
+        subprocess.call(
+            [
+                "ckan",
+                "--config=/etc/ckan/default/production.ini",
+                "search-index",
+                "rebuild",
+                message["Body"]
+            ]
+        )
+        # delete the message from the queue
+        client.delete_message(QueueUrl=sqs_url, ReceiptHandle = message["ReceiptHandle"]) 
             
 # this function gets called directly by cron, hence the if __name__ == "__main__"
 if __name__ == "__main__":
